37b78b2d48d798664e96124412c9c932ee441a67
natut <infinity.enoch@gmail.com>
2021-04-24 11:30:27 +0800

    ...

--- config.js
+++ config.js
@@ -8,14 +8,14 @@ const config = {
       client_secret: '41af3630-0022-496e-b93d-bd4cee90d8c8',
       authURL: 'https://auth.truelayer.com/',
       authURI: '?response_type=code&client_id={client_id}&scope=info%20accounts%20balance%20cards%20transactions%20direct_debits%20standing_orders%20offline_access&redirect_uri={redirect_uri}&providers=uk-ob-all%20uk-oauth-all',
-      redirect: 'http://localhost:'+sysport+'/callback'
+      redirect: 'http://13.212.84.244:'+sysport+'/callback'
     },
     sandbox: {
         client_id: 'sandbox-cons-f315b7',
         client_secret: '41af3630-0022-496e-b93d-bd4cee90d8c8',
         authURL: 'https://auth.truelayer-sandbox.com/',
         authURI: '?response_type=code&client_id={client_id}&scope=info%20accounts%20balance%20cards%20transactions%20direct_debits%20standing_orders%20offline_access&redirect_uri={redirect_uri}&providers=uk-ob-all%20uk-oauth-all',
-        redirect: 'http://localhost:'+sysport+'/callback'
+        redirect: 'http://13.212.84.244:'+sysport+'/callback'
         //13.212.84.244
   
     },
