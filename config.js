2b42e8a2a2a8ecfa9fc54c7a8200c11247bb5d6f
natut <infinity.enoch@gmail.com>
2021-04-24 15:32:20 +0800

    ...

--- config.js
+++ config.js
@@ -5,18 +5,19 @@ const config = {
     },
     live: {
       client_id: 'cons-f315b7',
-      client_secret: '41af3630-0022-496e-b93d-bd4cee90d8c8',
+      client_secret: '98f71940-a3f9-4683-b47c-9188e3e8bb9e',
       authURL: 'https://auth.truelayer.com/',
       authURI: '?response_type=code&client_id={client_id}&scope=info%20accounts%20balance%20cards%20transactions%20direct_debits%20standing_orders%20offline_access&redirect_uri={redirect_uri}&providers=uk-ob-all%20uk-oauth-all',
       redirect: 'http://13.212.84.244:'+sysport+'/callback'
+      //13.212.84.244
     },
     sandbox: {
         client_id: 'sandbox-cons-f315b7',
-        client_secret: '41af3630-0022-496e-b93d-bd4cee90d8c8',
+        client_secret: '98f71940-a3f9-4683-b47c-9188e3e8bb9e',
         authURL: 'https://auth.truelayer-sandbox.com/',
-        authURI: '?response_type=code&client_id={client_id}&scope=info%20accounts%20balance%20cards%20transactions%20direct_debits%20standing_orders%20offline_access&redirect_uri={redirect_uri}&providers=uk-ob-all%20uk-oauth-all',
-        redirect: 'http://13.212.84.244:'+sysport+'/callback'
-        //13.212.84.244
+        authURI: '?response_type=code&client_id={client_id}&scope=info%20accounts%20balance%20cards%20transactions%20direct_debits%20standing_orders%20offline_access&redirect_uri={redirect_uri}&providers=uk-ob-all%20uk-oauth-all%20uk-cs-mock',
+        redirect: 'http://localhost:'+sysport+'/callback'
+
   
     },
 
--- routes/layer.js
+++ routes/layer.js
@@ -12,18 +12,32 @@ const client_secret = "c9ca698e-ab6f-47c3-b38b-dc32e8e103c2"
 const config = require('../config.js');
 
 
+const isValidToken = (token)=>{
+  // preseting conditions here for valid token; also could be used as a routine to renew token if neccessary 
+
+  if (token=='null' || !token) {
+    console.log('here',token)
+    return false
+    }
+  
+  return true
+}
+
 router.get("/authorize", async(req,res)=>{
-  console.log('d')  
+ 
   base = (config.app.authURI
       .replace('{client_id}',config.app.client_id))
       .replace('{redirect_uri}',config.app.redirect)
 
       // authentication link
   return res.status(200).send({'status':1,'url':config.app.authURL+base})
+
 });
 
 router.get("/callback", async(req,res)=>{
+
     console.log('[Application]  Sending authentication request')
+
     var options = {
         'method': 'POST',
         'url': config.app.authURL+'connect/token',
@@ -38,19 +52,29 @@ router.get("/callback", async(req,res)=>{
         }
       };
 
+    console.log(options)
     request(options, function (error, response) {
 
-      console.log('[Application]  authentication response',response)
+      //console.log('[Application]  authentication response',response)
 
       if (error) {
-        console.log('Error]',error)
+        console.log('Error]',error,response.body)
         return res.redirect("/generrorpage.html")
       }
 
       let token = JSON.parse(response.body)
 
       console.log('[Application]  token received')
-      return res.redirect("/?access_token="+token.access_token)
+
+      if (token.error) {
+        console.log('[Issue] ',token)
+        // investigate: 
+        // a few moments ago there was error on user but for some reason without changing anything 
+        // issue is gone. 
+        return res.redirect('/generrorpage.html')
+      } else {
+        return res.redirect("/?access_token="+token.access_token)
+      }
     
     });
 
@@ -58,8 +82,16 @@ router.get("/callback", async(req,res)=>{
 
 router.get("/cards/transactions/:token/:id", async (req,res)=>{ 
 
+  
+  if ( !isValidToken(req.params.token)) {
+    console.log('[Issue] Token invalid',req.params.token);
+    return res.redirect("/generrorpage.html");
+  }
+
   console.log('[Application]  query card transaction(s)',response)
+
   var request = require('request');
+
     var options = {
     'method': 'GET',
     'url': config.app.authURL+'data/v1/cards/'+req.params.id+'/transactions',
@@ -88,9 +120,16 @@ router.get("/cards/transactions/:token/:id", async (req,res)=>{
 
 })
 
+
 router.get("/cards/:token", async (req,res)=>{ 
+  
+    if ( !isValidToken(req.params.token)) {
+      console.log('[Issue] Token invalid',req.params.token);
+      return res.redirect("/generrorpage.html");
+    }
 
     console.log('[Application]  query card(s)')
+
     var request = require('request');
     var options = {
       'method': 'GET',
@@ -100,6 +139,8 @@ router.get("/cards/:token", async (req,res)=>{
       }
     };
 
+    console.log(' ...',options)
+
     request(options, function (error, response) {
       
       if (error) {
@@ -108,15 +149,16 @@ router.get("/cards/:token", async (req,res)=>{
       }
     
       try {
+        let e = JSON.parse(response.body)
+
         console.log('[Application]  ... received')
         console.log(e.results)
-
         return res.status(200).send({'status':1,'results':e.results})
-  
+
       } catch(e) {
 
         console.log('[Issue]',e)
-
+        return res.status(200).send({'status':0,'results':{}})
       }
 
     });
